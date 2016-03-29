35fbe3b38acc89cbfbfb8231e3e86bef08cc7534
joneqian <qian.qing@aliyun.com>
2016-03-29 19:12:08 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '首页-乡货圈-家乡的味道' });
+});
+
+router.get('/rule-share', function(req, res, next) {
+  res.render('rule-share', { title: '商家规则-乡货圈-家乡的味道' });
+});
+
+router.get('/flow-share', function(req, res, next) {
+  res.render('flow-share', { title: '使用流程-乡货圈-家乡的味道' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: '关于乡货-乡货圈-家乡的味道' });
+});
+
+router.get('/rule-promise', function(req, res, next) {
+  res.render('rule-promise', { title: '服务承诺-乡货圈-家乡的味道' });
+});
+
+router.get('/flow-booking', function(req, res, next) {
+  res.render('flow-booking', { title: '订单管理-乡货圈-家乡的味道' });
+});
+
+router.get('/flow-cash', function(req, res, next) {
+  res.render('flow-cash', { title: '钱包提现-乡货圈-家乡的味道' });
+});
+
+router.get('/flow-publish', function(req, res, next) {
+  res.render('flow-publish', { title: '商品发布-乡货圈-家乡的味道' });
+});
+
+module.exports = router;
